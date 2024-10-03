4e17a4f7729dfccfc7a3540d597b3641c782d758
Gabriela Dodson <dodsongabi@gmail.com>
2024-10-03 15:27:14 -0400

    Create Employee class with properties and methods

---
+++ cc_8.js
@@ -0,0 +1,12 @@
+//Creating a Employee Class
+class Employee {
+    constructor(name, salary, position, department){
+        this.name = name;
+        this.salary = salary;
+        this.position = position;
+        this.department = department;
+    }
+getDetails(){
+    return `Name: ${this.name}, Position: ${this.position}, Salary: ${this.salary}`;
+}
+}
