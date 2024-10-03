d011b39cc95069e1d3a0a5fea36109b0e9e6adae
Gabriela Dodson <dodsongabi@gmail.com>
2024-10-03 15:39:55 -0400

    Create Department class to manage employees and calculate salary

--- cc_8.js
+++ cc_8.js
@@ -10,3 +10,25 @@ getDetails(){
     return `Name: ${this.name}, Position: ${this.position}, Salary: ${this.salary}`;
 }
 }
+//Creating a Department Class
+class Department {
+    constructor(name){
+        this.name = name;
+        this.employees = [];
+    }
+    addEmployee(employee){
+        if (employee instanceof Employee){
+            this.employees.push(employee);
+        } else {
+            console.error("Invalid Employee");
+        }
+    }
+    getDepartmentSalary() {
+        return this.employees.reduce((total, employee) => total + employee.salary, 0);
+    }
+    }
+//adding a department and employees 
+const MarketingDepartment = new Department ("Marketing");
+const Daffy = new Employee("Daffy Duck", 72000, "Marketing");
+const Bugs = new Employee("Bugs Bunny", 90000, "Marketing");
+const Porky = new Employee("Porky Pig", 65000, "Marketing");
